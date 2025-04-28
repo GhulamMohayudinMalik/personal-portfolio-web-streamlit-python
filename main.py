d69f11488adac0e56a33a6b7900e8c587c79083e
Ghulam Mohayudin <ghulammohayudinmalik@gmail.com>
2025-04-28 13:24:22 +0500

    Add profile picture and Create columns, image, and description.

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+col1, col2 = st.columns(2)
+
+
+with col1:
+    st.image('images/my_image.png', width=800)
+
+
+with col2:
+    st.title("Ghulam Mohayudin")
+    content = """
+    Hi, I am Ghulam Mohayudin! I am a programmer, cyber security student, and technology enthusiast. I am in BS Cyber Security and Digital Forensics 7th semester in Islamia University of Bahawalpur. I am currently enhancing my Python and Web Development skills, and will be working in a little time, INSHA'ALLAH. 
+    """
+    st.info(content)
